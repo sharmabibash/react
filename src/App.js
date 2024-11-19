71af52375923c5e4e06a9d225be5e74ef64c21a1
Bibash Sharma <sharmabibash77@gmail.com>
2024-11-19 15:53:34 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello BP CODING</h1>
+        <h2>Welcome to React Episode-02</h2>
+        <h3>Today we learn how to push on git and guthub this project</h3>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
